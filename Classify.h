a3ce1139752c1fa31a989307c95403ddd1b09c71
adi-ben-yehuda <adiby2017@gmail.com>
2023-01-12 11:24:09 +0200

    create the classify class

---
+++ Classify.h
@@ -0,0 +1,33 @@
+#ifndef ADVANCED_EX_4_CLASSIFY_H
+#define ADVANCED_EX_4_CLASSIFY_H
+
+#include <iostream>
+#include <map>
+#include "Command.h"
+
+class Classify : public Command {
+private:
+    int k;
+    string metric;
+    string pathTrain;
+    string pathTest;
+    map<int, string> classifications; // key = line number. value = classification.
+
+public:
+    Classify();
+
+    void setK(int x);
+    int getK();
+    void setMetric(string dist);
+    string getMetric();
+    void setPathTrain(string pathTrain);
+    string getPathTrain();
+    void setPathTest(string pathTest);
+    string getPathTest();
+    void execute();
+
+    ~Classify();
+};
+
+
+#endif //ADVANCED_EX_4_CLASSIFY_H
--- cmake-build-debug/CMakeFiles/advanced_ex_4.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for advanced_ex_4.
-# This may be replaced when dependencies are built.
