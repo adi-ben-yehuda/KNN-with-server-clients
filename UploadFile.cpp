facac5ad5a00b705c9156583e4fe7649d64b4e6e
Shahar Moshonov <shaharmosho@gmail.com>
2023-01-24 10:52:24 +0200

    fix the upload

--- UploadFile.cpp
+++ UploadFile.cpp
@@ -20,7 +20,7 @@ void UploadFile::execute() {
     ofstream File;
     string messageTrain = "Please upload your local train CSV file.\n",
             messageTest = "Please upload your local test CSV file.\n", complete = "Upload complete.\n", content = "";
-    char buffer[4096] = " ";
+    char buffer[4096] = "\000";
     bool close = false;
     bool firstTime = true;
 
@@ -32,56 +32,24 @@ void UploadFile::execute() {
 
     if (buffer[0] != '*') { // valid train
         File = createFile("train.csv");
-        if(File) {
+        if (File) {
             // Get all the data.s
             while (!close) {
                 if (!firstTime) {
+                    memset(buffer, '\000', 4096);
                     content = dio->read();
                     // copying the contents of the string to char array
                     strcpy(buffer, content.c_str());
                 }
                 firstTime = false;
                 for (int i = 0; i < 4096; ++i) {
-                    if (buffer[i] != '&') {
-                        File.put(buffer[i]);
-                    } else {
+                    if (buffer[i] == '&') { // End of data
                         close = true;
                         break;
-                    }
-                }
-            }
-
-            File.close();
-        }
-        // Send "Upload complete" message to the client.
-        dio->write(complete);
-
-        // Send messageTest to the client.
-        dio->write(messageTest);
-
-        memset(buffer, '\000', 4096);
-        close = false;
-
-        content = dio->read();
-        // copying the contents of the string to char array
-        strcpy(buffer, content.c_str());
-        firstTime = true;
-        if (buffer[0] != '*') { // valid train
-            File = createFile("test.csv");
-            // Get all the data.s
-            while (!close) {
-                if (!firstTime) {
-                    content = dio->read();
-                    // copying the contents of the string to char array
-                    strcpy(buffer, content.c_str());
-                }
-                firstTime = false;
-                for (int i = 0; i < 4096; ++i) {
-                    if (buffer[i] != '&') {
-                        File.put(buffer[i]);
-                    } else {
-                        close = true;
+                    } else if (buffer[i] == '\000') {
                         break;
+                    } else {
+                        File.put(buffer[i]);
                     }
                 }
             }
@@ -91,8 +59,49 @@ void UploadFile::execute() {
             // Send "Upload complete" message to the client.
             dio->write(complete);
 
-            data->setIsTrain(true);
-            data->setIsTest(true);
+            // Send messageTest to the client.
+            dio->write(messageTest);
+
+            memset(buffer, '\000', 4096);
+            close = false;
+
+            content = dio->read();
+            // copying the contents of the string to char array
+            strcpy(buffer, content.c_str());
+            firstTime = true;
+            if (buffer[0] != '*') { // valid train
+                File = createFile("test.csv");
+                if (File) {
+                    // Get all the data.s
+                    while (!close) {
+                        if (!firstTime) {
+                            memset(buffer, '\000', 4096);
+                            content = dio->read();
+                            // copying the contents of the string to char array
+                            strcpy(buffer, content.c_str());
+                        }
+                        firstTime = false;
+                        for (int i = 0; i < 4096; ++i) {
+                            if (buffer[i] == '&') { // End of data
+                                close = true;
+                                break;
+                            } else if (buffer[i] == '\000') {
+                                break;
+                            } else {
+                                File.put(buffer[i]);
+                            }
+                        }
+                    }
+
+                    File.close();
+
+                    // Send "Upload complete" message to the client.
+                    dio->write(complete);
+
+                    data->setIsTrain(true);
+                    data->setIsTest(true);
+                }
+            }
         }
     }
 }
